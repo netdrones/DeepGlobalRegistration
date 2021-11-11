Fix issue with point cloud scaling

core/deep_global_registration.py
@@ -30,19 +30,19 @@ def registration_ransac_based_on_feature_matching(pcd0, pcd1, feats0, feats1,
                                                   distance_threshold, num_iterations):
   assert feats0.shape[1] == feats1.shape[1]
 
-  source_feat = o3d.registration.Feature()
+  source_feat = o3d.pipelines.registration.Feature()
   source_feat.resize(feats0.shape[1], len(feats0))
   source_feat.data = feats0.astype('d').transpose()
 
-  target_feat = o3d.registration.Feature()
+  target_feat = o3d.pipelines.registration.Feature()
   target_feat.resize(feats1.shape[1], len(feats1))
   target_feat.data = feats1.astype('d').transpose()
 
-  result = o3d.registration.registration_ransac_based_on_feature_matching(
+  result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
       pcd0, pcd1, source_feat, target_feat, distance_threshold,
-      o3d.registration.TransformationEstimationPointToPoint(False), 4,
-      [o3d.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)],
-      o3d.registration.RANSACConvergenceCriteria(num_iterations, 1000))
+      o3d.pipelines.registration.TransformationEstimationPointToPoint(False), 4,
+      [o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)],
+      o3d.pipelines.registration.RANSACConvergenceCriteria(num_iterations, 1000))
 
   return result.transformation
 
@@ -52,10 +52,8 @@ def registration_ransac_based_on_correspondence(pcd0, pcd1, idx0, idx1,
   corres = np.stack((idx0, idx1), axis=1)
   corres = o3d.utility.Vector2iVector(corres)
 
-  result = o3d.registration.registration_ransac_based_on_correspondence(
-      pcd0, pcd1, corres, distance_threshold,
-      o3d.registration.TransformationEstimationPointToPoint(False), 4,
-      o3d.registration.RANSACConvergenceCriteria(4000000, num_iterations))
+  result = o3d.pipelines.registration.registration_ransac_based_on_correspondence(
+      pcd0, pcd1, corres, distance_threshold)
 
   return result.transformation
 